4fbedf38ad406603bb178e40cb42870a29a72bd9
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-11 19:31:56 -0300

    feat: Add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,20 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { theme } from '../styles/theme'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
