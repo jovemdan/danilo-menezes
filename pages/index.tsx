895e064a482a9f7da2d75a4b2aca8d79b2611549
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-12 22:28:02 -0300

    fix: Adjusts layout index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import { Hi } from '../components/hi/hi'
+import { Navbar } from '../components/navbar'
 export default function Home() {
   return (
-    <>
+    <Box maxWidth='860px' height='100vh' margin='0 auto' px={'10'}>
+      <Navbar />
       <Hi />
-    </>
+    </Box>
   )
 }
