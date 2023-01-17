13f75b55b399c01b0a2c750fc9e6b5a910699714
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-16 21:47:10 -0300

    feat: Update url

--- components/navbar/nav-desktop.tsx
+++ components/navbar/nav-desktop.tsx
@@ -1,13 +1,23 @@
 import { Flex, HStack } from '@chakra-ui/react'
 
 import { ActiveLink } from './active-link'
+import { Logo } from './logo'
 
 export function NavDesktop() {
   return (
-    <Flex as='nav' mt={8} gap='52' justifyContent='flex-start'>
-      <ActiveLink href='/'>Home</ActiveLink>
-      <ActiveLink href='/posts'>Projects</ActiveLink>
-      <ActiveLink href='/projects'>Blog</ActiveLink>
+    <Flex
+      mt={8}
+      maxW='854px'
+      gap='200px'
+      alignItems='center'
+      alignContent='center'
+    >
+      <Logo />
+      <Flex as='nav' gap='78px' justifyContent='center'>
+        <ActiveLink href='/'>Home</ActiveLink>
+        <ActiveLink href='/projects'>Projects</ActiveLink>
+        <ActiveLink href='/blog'>Blog</ActiveLink>
+      </Flex>
     </Flex>
   )
 }
