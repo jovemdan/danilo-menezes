77796823bcb95236e79fa8eef8f67fb6b5a1142c
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-17 22:10:50 -0300

    feat: Add component SocialIcons

--- components/navbar/nav-mobile.tsx
+++ components/navbar/nav-mobile.tsx
@@ -13,6 +13,7 @@ import {
   Link,
   Image,
 } from '@chakra-ui/react'
+import { SocialIcons } from '../hi/social-icons'
 import { ActiveLink } from './active-link'
 import { Logo } from './logo'
 
@@ -60,20 +61,7 @@ export function HeaderMobile() {
               </ActiveLink>
             </VStack>
 
-            <Flex gap='5' pt='6'>
-              <Link href='https://github.com/jovemdan' target='_blank'>
-                <Image src='/github.svg' alt='github' cursor='pointer' />
-              </Link>
-              <Link href='https://twitter.com/jovemdan_' target='_blank'>
-                <Image src='/twitter.svg' alt='twitter' cursor='pointer' />
-              </Link>
-              <Link
-                href='https://www.linkedin.com/in/danilo-gon%C3%A7alves-a862a6142/'
-                target='_blank'
-              >
-                <Image src='/linkedin.svg' alt='linkedin' cursor='pointer' />
-              </Link>
-            </Flex>
+            <SocialIcons />
           </DrawerBody>
         </DrawerContent>
       </Drawer>
