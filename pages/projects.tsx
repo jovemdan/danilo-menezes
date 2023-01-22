cf48a1236dadaa1f7f91f766e4fc8f4a4548e2da
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-22 11:34:29 -0300

    feat: Inserting new props

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Image } from '@chakra-ui/react'
+import { Box, Flex, Text, Image, useBreakpointValue } from '@chakra-ui/react'
 import { Navbar } from '../components/navbar'
 import { ProjectDescription } from '../components/projects'
 import { SelectedProjects } from '../components/projects/selected-projects'
@@ -8,23 +8,22 @@ export default function Projects() {
     <>
       <Box maxWidth='860px' margin='0 auto' px={'10'}>
         <Navbar />
+        <SelectedProjects />
         <ProjectDescription
-          year=' 2022 - Now'
-          name='Bitrefill'
-          description='I currently work at Bitrefill, mainly focusing on search &
-              discovery. I also lead design & development of the design system.'
+          year=' 2023 - Now'
+          name='Danilo Menezes'
+          description='I developed this site to centralize my personal projects, write articles and improve my skills. I used Next js and Chakra Ui.'
+          image='/projects/danilomenezes.png'
+          githubUrl='https://github.com/jovemdan/danilo-menezes'
+          projectUrl='https://danilo-menezes.vercel.app/'
         />
         <ProjectDescription
-          year=' 2022 - Now'
-          name='Bitrefill'
-          description='I currently work at Bitrefill, mainly focusing on search &
-              discovery. I also lead design & development of the design system.'
-        />
-        <ProjectDescription
-          year=' 2022 - Now'
-          name='Bitrefill'
-          description='I currently work at Bitrefill, mainly focusing on search &
-              discovery. I also lead design & development of the design system.'
+          year=' 2021 - 2022'
+          name='My Finances'
+          description='I made some contributions in this project together with my friend. In this system it is possible to control your finances in a simple way. We use Next js, Chakra Ui and MongoDb.'
+          image='/projects/myfinances.png'
+          githubUrl='https://github.com/darkstudio-apps/my-finances'
+          projectUrl='https://www.my-finances.app/'
         />
         <Box as='footer' height='50px'></Box>
       </Box>
