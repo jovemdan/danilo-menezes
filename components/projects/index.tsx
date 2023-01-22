4be205eccd90f58e93d654dc3ea623a26a699e41
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-22 11:33:34 -0300

    feat: Implementing hover and image projects

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,28 +1,35 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
-import { SelectedProjects } from './selected-projects'
+import { Box, Flex, Text, Image } from '@chakra-ui/react'
+import Link from 'next/link'
 
 interface ProjectDescriptionProps {
   year: string
   name: string
   description: string
   image?: string
+  imageMobile?: string
+  githubUrl: string
+  projectUrl: string
 }
 export function ProjectDescription({
   year,
   name,
   description,
+  image,
+  imageMobile,
+  githubUrl,
+  projectUrl,
 }: ProjectDescriptionProps) {
   return (
     <>
-      <SelectedProjects />
       <Flex
-        mt={19}
+        mt={25}
         gap={['20px', '20px', '87px']}
         flexDirection={['column', 'column', 'row']}
+        cursor='pointer'
       >
         <Box>
           <Text
-            w='135px'
+            w='140px'
             fontSize={['15px', '20px', '24px']}
             fontWeight='700'
             lineHeight='36px'
@@ -36,6 +43,9 @@ export function ProjectDescription({
             color='white'
             fontWeight='700'
             fontSize={['14px', '15px', '16px']}
+            cursor='pointer'
+            transition='0.9s'
+            _hover={{ color: '#FF9900' }}
           >
             {name}
           </Text>
@@ -43,30 +53,46 @@ export function ProjectDescription({
             as='p'
             fontWeight='700'
             fontSize={['14px', '15px', '16px']}
-            color='#9A9A9A'
+            color='white'
+            opacity={0.6}
             alignItems='left'
             mt='2px'
+            transition='0.9s'
+            _hover={{ opacity: 0.9 }}
           >
             {description}
           </Text>
-          <Text
-            fontWeight='700'
-            fontSize={['14px', '15px', '16px']}
-            color='#FF9900'
-            mt='10px'
-          >
-            Read case study
-          </Text>
+          <Link href={githubUrl} target='_blank'>
+            <Text
+              fontWeight='700'
+              fontSize={['14px', '15px', '16px']}
+              color='#FF9900'
+              mt='10px'
+              transition='0.9s'
+              _hover={{ opacity: 0.5 }}
+            >
+              View in Github
+            </Text>
+          </Link>
 
-          <Box
-            mt='16px'
-            w='100%'
-            height='285px'
-            bgColor='#B476A6'
-            borderRadius='9px'
-          ></Box>
+          <Link href={projectUrl} target='_blank'>
+            <Image
+              src={image}
+              alt={image}
+              mt='16px'
+              w={['100%']}
+              height='285px'
+              bgSize='cover'
+              objectFit='cover'
+              bgRepeat='no-repeat'
+              borderRadius='9px'
+              transition='0.9s'
+              _hover={{ opacity: 0.5 }}
+            />
+          </Link>
         </Box>
       </Flex>
+      <Box h='50px'></Box>
     </>
   )
 }
