da546c01ac786058c6294425042715e62e615e09
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-30 17:30:50 -0300

    feat: Include Box and Navbar in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { Navbar } from '../components/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Box maxWidth='860px' margin='0 auto' px={'10'}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Box as='footer' height='50px'></Box>
+      </Box>
     </ChakraProvider>
   )
 }
