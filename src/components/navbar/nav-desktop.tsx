b7dc61d32528b87c0ae7f4c9ef1fcbb202698e5b
jovemdan <danilogdev@gmail.com>
2023-03-19 22:51:36 -0300

    feat: New layout in nav-bar desktop

--- src/components/navbar/nav-desktop.tsx
+++ src/components/navbar/nav-desktop.tsx
@@ -6,13 +6,13 @@ export function NavDesktop() {
   return (
     <Flex
       mt={8}
-      maxW='854px'
-      gap='200px'
+      maxW='724x'
+      gap='192px'
       alignItems='center'
       alignContent='center'
     >
       <Logo />
-      <Flex as='nav' gap='80px' justifyContent='center'>
+      <Flex as='nav' gap='128px' justifyContent='center'>
         <ActiveLink href='/'>Home</ActiveLink>
         <ActiveLink href='/projects'>Projects</ActiveLink>
         <ActiveLink href='/blog'>Blog</ActiveLink>
