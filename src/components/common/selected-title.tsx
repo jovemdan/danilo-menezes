e8a872ef0687baaa13cf8f1123f6a25738d10977
jovemdan <danilogdev@gmail.com>
2023-04-07 15:05:52 -0300

    feat: Created component SelectedTitle

---
+++ src/components/common/selected-title.tsx
@@ -0,0 +1,23 @@
+import { Flex, Image, Text } from '@chakra-ui/react'
+
+interface SelectedTitleProps {
+  title: string
+}
+export function SelectedTitle({ title }: SelectedTitleProps) {
+  return (
+    <Flex mt={['10', '12', '32']} gap='3'>
+      <Image src='/divider-small.svg' alt='divider' />
+      <Text
+        fontSize={['15px', '20px', '24px']}
+        fontWeight='700'
+        lineHeight='36px'
+        color='white'
+        cursor='pointer'
+        transition='0.9s'
+        _hover={{ color: '#FF9900' }}
+      >
+        {title}
+      </Text>
+    </Flex>
+  )
+}
