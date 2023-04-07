2bad7096d973a6d1557121dd86751fa742700189
jovemdan <danilogdev@gmail.com>
2023-04-07 15:52:00 -0300

    feat: Coments posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,7 +5,7 @@ export default function Blog() {
   return (
     <>
       <SelectedTitle title='Selected posts' />
-      <Box mt={[25, 25, 28]} maxW='660px'>
+      {/* <Box mt={[25, 25, 28]} maxW='660px'>
         <InputGroup>
           <InputRightElement
             pointerEvents='none'
@@ -37,7 +37,7 @@ export default function Blog() {
           </Box>
           <Text fontSize='14px' color='#9A9A9A' >Dec 17, 2021</Text>
         </Flex>
-      </Flex>
+      </Flex> */}
     </>
   )
 }
