eef2814bd711380b13fe3da71d2240db175f8c3d
jovemdan <danilogdev@gmail.com>
2023-04-15 17:30:22 -0300

    feat: Move blog pages to path

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,44 @@
+import { SearchIcon } from '@chakra-ui/icons'
+import { Box, Flex, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import { SelectedTitle } from '../../components/common/selected-title'
+export default function Blog() {
+  return (
+    <>
+      <SelectedTitle title='Selected posts' />
+      <Box mt={[25, 16]} maxW='660px'>
+        <InputGroup>
+          <InputRightElement
+            pointerEvents='none'
+          >
+            <SearchIcon color='gray.300' />
+          </InputRightElement>
+          <Input type='tel' placeholder='Search posts...' color={'gray.200'} focusBorderColor='#FF9900' />
+        </InputGroup>
+      </Box>
+      <Flex mt={[16]} maxW={['450px', '660px']} flexDirection='column' gap='30px'>
+        <Flex justifyContent='space-between' cursor='pointer' >
+          <Link href={'/blog/artigo'}>
+            <Text fontSize={'14px'} color='white' _hover={{ color: '#FF9900' }} transition='0.9s'>Building a mapping platform with React and Mapbox</Text>
+            <Text fontSize={'14px'} color='#9A9A9A'>The intersection observer web API</Text>
+          </Link>
+          <Text fontSize={['10px', '14px']} display={['none', 'none', 'block']} color='#9A9A9A' >Dec 17, 2021</Text>
+        </Flex>
+        <Flex justifyContent='space-between' cursor='pointer'>
+          <Box>
+            <Text fontSize={'14px'} color='white' _hover={{ color: '#FF9900' }} transition='0.9s'>Building a mapping platform with React and Mapbox</Text>
+            <Text fontSize={'14px'} color='#9A9A9A'>The intersection observer web API</Text>
+          </Box>
+          <Text fontSize={['10px', '14px']} display={['none', 'none', 'block']} color='#9A9A9A' >Dec 17, 2021</Text>
+        </Flex>
+        <Flex justifyContent='space-between' cursor='pointer'>
+          <Box>
+            <Text fontSize={'14px'} color='white' _hover={{ color: '#FF9900' }} transition='0.9s'>Building a mapping platform with React and Mapbox</Text>
+            <Text fontSize={'14px'} color='#9A9A9A'>The intersection observer web API</Text>
+          </Box>
+          <Text fontSize={['10px', '14px']} display={['none', 'none', 'block']} color='#9A9A9A' >Dec 17, 2021</Text>
+        </Flex>
+      </Flex>
+    </>
+  )
+}
