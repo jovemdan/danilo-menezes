2cfa433f5e72af9a31d1362c0b7998f12a62c80f
jovemdan <danilogdev@gmail.com>
2023-12-30 13:45:26 -0300

    fear: Created blog context

---
+++ src/contexts/BlogContext.tsx
@@ -0,0 +1,111 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import { api } from '../lib/axios'
+
+interface BlogProviderProps {
+  children: ReactNode
+}
+
+interface PostsType {
+  id: number
+  user: string
+  number: number
+  title: string
+  content: string
+  date: string
+  comments: string
+  url: string
+}
+
+interface BlogContextProps {
+  posts: PostsType[]
+  totalPosts: number
+  postById: PostsType
+  fetchAllPosts: () => Promise<void>
+  fetchPostById: (id: number) => Promise<void>
+  searchPost: (content: string) => Promise<void>
+}
+
+export const BlogContext = createContext({} as BlogContextProps)
+
+export function BlogProvider({ children }: BlogProviderProps) {
+  const [posts, setPosts] = useState<PostsType[]>([])
+  const [postById, setPostById] = useState({} as PostsType)
+  const [totalPosts, setTotalPosts] = useState(0)
+
+  async function fetchAllPosts() {
+    const { data } = await api.get('/search/issues', {
+      params: {
+        q: 'repo:pceolato/ignite-challenge-github-blog'
+      }
+    }
+    )
+
+    const postsTotal: PostsType[] = []
+
+    data.items.map((post: any) => {
+      postsTotal.push({
+        id: post.id,
+        user: post.user.login,
+        number: post.number,
+        title: post.title,
+        content: post.body,
+        date: post.created_at,
+        comments: post.comments,
+        url: post.html_url
+      })
+    })
+
+    setPosts(postsTotal)
+    setTotalPosts(data.total_count)
+  }
+
+  async function fetchPostById(number: number) {
+    setPostById({} as PostsType)
+    console.log('number', number)
+    const { data } = await api.get(`/repos/pceolato/ignite-challenge-github-blog/issues/${number}`)
+    console.log('data', data)
+    setPostById({
+      id: data.id,
+      user: data.user.login,
+      number: data.number,
+      title: data.title,
+      content: data.body,
+      date: data.created_at,
+      comments: data.comments,
+      url: data.html_url
+    })
+  }
+
+  async function searchPost(content: string) {
+    const { data } = await api.get('/search/issues', {
+      params: {
+        q: `${content}repo:pceolato/ignite-challenge-github-blog`
+      }
+    }
+    )
+
+    const postsTotal: PostsType[] = []
+
+    data.items?.map((post: any) => {
+      postsTotal.push({
+        id: post.id,
+        user: post.user.login,
+        number: post.number,
+        title: post.title,
+        content: post.body,
+        date: post.created_at,
+        comments: post.comments,
+        url: post.html_url
+      })
+    })
+
+    setPosts(postsTotal)
+    setTotalPosts(data.total_count)
+  }
+
+  return (
+    <BlogContext.Provider value={{ posts, totalPosts, fetchPostById, postById, searchPost, fetchAllPosts }}>
+      {children}
+    </BlogContext.Provider>
+  )
+}
