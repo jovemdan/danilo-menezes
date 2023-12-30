1fe09e146e117fbccd1d19bbb8a1c51cc93455ae
jovemdan <danilogdev@gmail.com>
2023-12-30 13:46:09 -0300

    feat: Added BlogProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import type { AppProps } from 'next/app'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { Navbar } from '../components/navbar'
+import { BlogProvider } from '../contexts/BlogContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Box maxWidth='860px' margin='0 auto' px={'10'}>
-        <Navbar />
-        <Component {...pageProps} />
-        <Box as='footer' height='50px'></Box>
-      </Box>
+      <BlogProvider>
+        <Box maxWidth='860px' margin='0 auto' px={'10'}>
+          <Navbar />
+          <Component {...pageProps} />
+          <Box as='footer' height='50px'></Box>
+        </Box>
+      </BlogProvider>
+
     </ChakraProvider>
   )
 }
