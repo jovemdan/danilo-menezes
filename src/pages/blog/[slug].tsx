4d349f8f1914bd8810f0d3d0b6c169db4d3d44d2
jovemdan <danilogdev@gmail.com>
2023-12-30 13:46:35 -0300

    feat: Updated BlogDetails

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,5 +1,23 @@
-export default function BlogDetails() {
+import { GetServerSideProps } from 'next'
+import { Atrticle } from '../../components/articles/Article'
+
+interface BlogDetailsProps {
+  slug: string
+}
+export default function BlogDetails({ slug }: BlogDetailsProps) {
   return (
-    <></>
+    <>
+      <Atrticle image='/articles/image.png' slug={slug} />
+    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const slug = context.query.slug
+
+  return {
+    props: {
+      slug
+    }
+  }
 }
