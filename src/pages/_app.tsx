65c5526f9dd511b7cf9ef4fa0ca7bb8b94bdc978
jovemdan <danilogdev@gmail.com>
2024-01-02 19:05:33 -0300

    feat: Added router eventes and ng progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,24 @@ import { Box, ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { Navbar } from '../components/navbar'
 import { BlogProvider } from '../contexts/BlogContext'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => {
+      NProgress.start()
+    })
+    Router.events.on('routeChangeComplete', () => {
+      NProgress.done()
+    })
+    Router.events.on('routeChangeError', () => {
+      NProgress.done()
+    })
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <BlogProvider>
