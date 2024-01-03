c9aa7afabd386de56fc3ae3d1a14823a522cc0af
jovemdan <danilogdev@gmail.com>
2024-01-03 19:06:53 -0300

    feat: Added animation in ProjectDescription

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Text, Image } from '@chakra-ui/react'
 import Link from 'next/link'
+import { Fade } from 'react-awesome-reveal'
 
 interface ProjectDescriptionProps {
   year: string
@@ -20,97 +21,99 @@ export function ProjectDescription({
 }: ProjectDescriptionProps) {
   return (
     <>
-      <Flex
-        mt={[25, 25, 28]}
-        gap={['20px', '20px', '87px']}
-        flexDirection={['column', 'column', 'row']}
-        cursor='pointer'
-      >
-        <Box>
-          <Text
-            w='140px'
-            fontSize={['15px', '20px', '24px']}
-            fontWeight='700'
-            lineHeight='36px'
-            color='#9A9A9A'
-          >
-            {year}
-          </Text>
-        </Box>
-        <Box maxW='450px'>
-          <Link href={projectUrl} target='_blank'>
+      <Fade delay={1e2} cascade damping={1e-1}>
+        <Flex
+          mt={[25, 25, 28]}
+          gap={['20px', '20px', '87px']}
+          flexDirection={['column', 'column', 'row']}
+          cursor='pointer'
+        >
+          <Box>
             <Text
-              color='white'
+              w='140px'
+              fontSize={['15px', '20px', '24px']}
               fontWeight='700'
-              fontSize={['14px', '15px', '16px']}
-              cursor='pointer'
-              transition='0.9s'
-              _hover={{ color: '#FF9900' }}
+              lineHeight='36px'
+              color='#9A9A9A'
             >
-              {name}
+              {year}
             </Text>
-            <Text
-              as='p'
-              fontWeight='400'
-              fontSize={['14px', '15px', '16px']}
-              color='white'
-              opacity={0.6}
-              alignItems='left'
-              mt='2px'
-              transition='0.9s'
-              _hover={{ opacity: 0.9 }}
-            >
-              {description}
-            </Text>
-          </Link>
-          {
-            githubUrl ? (
-              <Link href={githubUrl} target='_blank'>
-                <Text
-                  fontWeight='700'
-                  fontSize={['14px', '15px', '16px']}
-                  color='#FF9900'
-                  mt='10px'
-                  transition='0.9s'
-                  _hover={{ opacity: 0.5 }}
-                >
-                  View in Github
-                </Text>
-              </Link>
-            )
-              :
-              <Link href={githubUrl} target='_blank'>
-                <Text
-                  fontWeight='700'
-                  fontSize={['14px', '15px', '16px']}
-                  color='#FF9900'
-                  mt='10px'
-                  transition='0.9s'
-                  _hover={{ opacity: 0.5 }}
-                >
-                  View in Web
-                </Text>
-              </Link>
-          }
+          </Box>
+          <Box maxW='450px'>
+            <Link href={projectUrl} target='_blank'>
+              <Text
+                color='white'
+                fontWeight='700'
+                fontSize={['14px', '15px', '16px']}
+                cursor='pointer'
+                transition='0.9s'
+                _hover={{ color: '#FF9900' }}
+              >
+                {name}
+              </Text>
+              <Text
+                as='p'
+                fontWeight='400'
+                fontSize={['14px', '15px', '16px']}
+                color='white'
+                opacity={0.6}
+                alignItems='left'
+                mt='2px'
+                transition='0.9s'
+                _hover={{ opacity: 0.9 }}
+              >
+                {description}
+              </Text>
+            </Link>
+            {
+              githubUrl ? (
+                <Link href={githubUrl} target='_blank'>
+                  <Text
+                    fontWeight='700'
+                    fontSize={['14px', '15px', '16px']}
+                    color='#FF9900'
+                    mt='10px'
+                    transition='0.9s'
+                    _hover={{ opacity: 0.5 }}
+                  >
+                    View in Github
+                  </Text>
+                </Link>
+              )
+                :
+                <Link href={githubUrl} target='_blank'>
+                  <Text
+                    fontWeight='700'
+                    fontSize={['14px', '15px', '16px']}
+                    color='#FF9900'
+                    mt='10px'
+                    transition='0.9s'
+                    _hover={{ opacity: 0.5 }}
+                  >
+                    View in Web
+                  </Text>
+                </Link>
+            }
 
 
-          <Link href={projectUrl} target='_blank'>
-            <Image
-              src={image}
-              alt={image}
-              mt='16px'
-              w={['100%']}
-              height='285px'
-              bgSize='cover'
-              objectFit='cover'
-              bgRepeat='no-repeat'
-              borderRadius='9px'
-              transition='0.9s'
-              _hover={{ opacity: 0.5 }}
-            />
-          </Link>
-        </Box>
-      </Flex>
+            <Link href={projectUrl} target='_blank'>
+              <Image
+                src={image}
+                alt={image}
+                mt='16px'
+                w={['100%']}
+                height='285px'
+                bgSize='cover'
+                objectFit='cover'
+                bgRepeat='no-repeat'
+                borderRadius='9px'
+                transition='0.9s'
+                _hover={{ opacity: 0.5 }}
+              />
+            </Link>
+          </Box>
+        </Flex>
+      </Fade>
       <Box h='50px'></Box>
     </>
   )
